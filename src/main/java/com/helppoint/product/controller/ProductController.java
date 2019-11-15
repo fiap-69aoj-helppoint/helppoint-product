b27071e689846088ec5f1f7b08c83a54b298bf14
Flávio Oliveira <flaoliveira@uolinc.com>
2019-11-15 10:56:07 -0300

    ajustes configurações

--- src/main/java/com/helppoint/product/controller/ProductController.java
+++ src/main/java/com/helppoint/product/controller/ProductController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import java.util.List;
 @Api(value = "Produtos", tags = {"API de Produtos"})
 @RestController
 @RequestMapping("/products")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
 
     @Autowired
